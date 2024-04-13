6f60a9bd0d2d756fbc54b9e9a1653f81e03f0444
Frost-704 <boolean.pg.me@gmail.com>
2024-04-13 17:36:23 +0300

    feat: implement getFirstResolvedPromiseResult solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -59,8 +59,16 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return Promise.allSettled(promises).then((responses) => {
+    const resolvedPromises = responses.filter(
+      (response) => response.status === 'fulfilled'
+    );
+    if (resolvedPromises.length > 0) {
+      return resolvedPromises[0].value;
+    }
+    throw new Error('Promise rejected');
+  });
 }
 
 /**
